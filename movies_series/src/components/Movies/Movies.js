7522a2b949563207dcc9a543c2de0cdea2dcb6a4
SBellido <sebastianbellidodg@gmail.com>
2023-10-31 17:45:11 +0100

    Rendering Movies

--- movies_series/src/components/Movies/Movies.js
+++ movies_series/src/components/Movies/Movies.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { getMovies } from '../../services/database';
 
-function Movies() {
+const Movies = () => {
+  const [moviesData, setMoviesData] = useState([]);
+
   const handleClick = () => {
     window.history.go(-1);
   };
 
+  useEffect(() => {
+    // Obtén los datos de la base de datos en lugar de hacer una solicitud HTTP
+    const data = getMovies();
+
+    // Filtra, ordena y obtén los primeros 20 resultados
+    const filteredData = data
+      .filter(item => item.releaseYear >= 2010 && item.programType === 'movie')
+      .sort((a, b) => a.title.localeCompare(b.title))
+      .slice(0, 20);
+
+    setMoviesData(filteredData);
+  }, []);
+
   return (
-    <div>
-      <h1>Películas</h1>
-      <button onClick={handleClick}>
-        Volver
-      </button>
-    </div>
+    <React.Fragment>
+      <button onClick={handleClick}>Volver</button>
+      <div>
+        {moviesData.map(item => (
+          <div key={item.title} className="result-box">
+            <img src={item.images['Poster Art'].url} alt={item.title} />
+            <h3>{item.title}</h3>
+          </div>
+        ))}
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 
 export { Movies };
