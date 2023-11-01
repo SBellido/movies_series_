6cf4ce46b70e348417e12b20393f028dc10dab70
SBellido <sebastianbellidodg@gmail.com>
2023-11-01 14:52:49 +0100

    Modal was added in Series

--- movies_series/src/components/Programs/Series/SeriesList.js
+++ movies_series/src/components/Programs/Series/SeriesList.js
@@ -1,22 +1,42 @@
 import React, {  useState, useEffect } from 'react';
 import { getSeries } from '../../../services/database';
+import CloseIcon from '@mui/icons-material/Close';
 import '../Programs.css';
 
 const SeriesList = () => {
-  const [seriesData, setSeriesData] = useState([]);
+  const [seriesData, setSerieData] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [serieSelected, setSerieSelected] = useState(null);
 
   useEffect(() => {
-    // Obtiene datos del archivo que simula una base datos en lugar de hacer una solicitud HTTP
     const data = getSeries();
-
-    // Filtra, ordena y obtén los primeros 20 resultados
     const filteredData = data
       .filter(item => item.releaseYear >= 2010 && item.programType === 'series')
       .sort((a, b) => a.title.localeCompare(b.title))
       .slice(0, 20);
 
-    setSeriesData(filteredData);
+    setSerieData(filteredData);
   }, []);
+
+  useEffect(() => {
+    const data = getSeries();
+    const filteredData = data
+      .filter(item => item.releaseYear >= 2010 && item.programType === 'series')
+      .sort((a, b) => a.title.localeCompare(b.title))
+      .slice(0, 20);
+
+    setSerieData(filteredData);
+  }, []);
+
+  const openModal = (movie) => {
+    setSerieSelected(movie);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSerieSelected(null);
+    setModalOpen(false);
+  };
 
   return (
     <React.Fragment>
@@ -25,7 +45,8 @@ const SeriesList = () => {
       </div>
       <div className="Grid-container">
         {seriesData.map(item => (
-          <div key={item.title} className="Grid-item">
+          <div key={item.title} className="Grid-item"
+            onClick={() => openModal(item)}>
             <img className="Image-product" src={item.images['Poster Art'].url} alt={item.title} />
             <div className='Containar-title--product'>
               <span>{item.title}</span>
@@ -33,6 +54,30 @@ const SeriesList = () => {
           </div>
         ))}
       </div>
+
+      {modalOpen && serieSelected && (
+        <div className="Modal">
+          <div className="Modal-container">
+            <img className="Image-modal"
+              src={serieSelected.images['Poster Art'].url} 
+              alt={serieSelected.title} />
+            <div className='Contenier-info--modal'>
+              <div className="Container-close--modal">
+                <CloseIcon 
+                  className="Close-modal" onClick={closeModal}
+                  data-testid="DeleteIcon">
+                </CloseIcon >
+              </div>
+              <div className='Container-text--modal'>
+                <h3 className='Title-modal'>{serieSelected.title}</h3>
+                <span>Año: {serieSelected.releaseYear}</span>
+                <p className='Sinapsis'><span>Sinapsis:</span></p> 
+                <p>{serieSelected.description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </React.Fragment>
   );
 };
